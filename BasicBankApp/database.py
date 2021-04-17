29f33cc73060104f932d9d90b833c2e24d81cf5c
Claire Munyole <munyolec@gmail.com>
2021-04-17 14:31:41 +0300

    update delete function

--- BasicBankApp/database.py
+++ BasicBankApp/database.py
@@ -4,19 +4,23 @@
 #delete record
 
 #find user 
+import os #allows for the deletion of a file
+user_db_path="BasicBankApp/data/user_record/"
 
 def create(account_number,user_details):
 
     completion_state=False
 
     try:
-        f= open("BasicBankApp/data/user_record/" +str(account_number) + ".txt","x")
+        f=open(user_db_path +str(account_number) + ".txt","x")
         
     except FileExistsError:
         
         print("That user already exists")
         #delete the already created file, and print out an error, then return false
-        return completion_state
+        #check file contents before deleting
+
+        # delete(account_number)
         f.close()
 
     else:
@@ -28,8 +32,6 @@ def create(account_number,user_details):
     #     f.close()
 
     return completion_state
-
-
 
     #create a file
     #name of the file would be an account_number.txt
@@ -52,7 +54,21 @@ def read(user_acc_number):
     #fetch the file content
 
 def delete(user_acc_number):
-    print("delete user record")
+
+    is_delete_successful = False
+    
+    if os.path.exists(user_db_path + str(user_acc_number) + ".txt"):
+       
+        try:
+
+            os.remove(user_db_path + str(user_acc_number) + ".txt")
+            is_delete_successful=True
+        except FileNotFoundError:
+            print("Users not found")  
+        finally:
+            return is_delete_successful
+
+
     #find user with acc number
     #delete the user record (file)
     #return true
@@ -61,5 +77,5 @@ def find(user_acc_number):
     print("find user")
     #find user record in the data folder
     
-
 # create(3567909922,['Claire','Munyole','munyolec@gmail.com','password', 200])
+print(delete(2715789230))
